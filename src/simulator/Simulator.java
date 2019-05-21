44ea6f47eeb387652ae830467e91d93185b4ccab
BartoszRudnik <rudnik49@gmail.com>
2019-05-21 17:13:28 +0200

    Koniec testu

--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -18,12 +18,5 @@ public class Simulator{
 
         Building building = new Building( numberOfFloors, numberOfElevators);
         Control control = new Control();
-        ReadData data = new ReadData();
-
-        /** Test klasy ReadData */
-        System.out.println(data.GetTime(0));
-        System.out.println(data.GetStartFloor(0));
-        System.out.println(data.GetTargetFloor(0));
-        System.out.println(data.GetIsPrivileged(0));
     }
 }
