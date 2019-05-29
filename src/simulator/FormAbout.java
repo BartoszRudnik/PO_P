1cf6ebfddb24f50b2b66f5d36c80e8a3a16e9627
Stanislaw Franczyk <sfranczyk5@gmail.com>
2019-05-29 23:09:32 +0200

    Elevator Table

--- src/simulator/FormAbout.java
+++ src/simulator/FormAbout.java
@@ -2,14 +2,29 @@ package simulator;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class FormAbout extends JFrame{
 
+    private List <JTextField> listFiledsElevator = new ArrayList<>();
+    private List <JTextField> listFiledsNumberOfPassengers = new ArrayList<>();
+    private List <JTextField> listFiledsTargetFloor = new ArrayList<>();
+    private List <JTextField> getListFiledsFloors = new ArrayList<>();
 
-    private String time; // zmienna pomocnicza??????????? Co to zmienna pomocnicza????
+    private JLabel labelInformation;
+    private JLabel labelPassengers;
+    private JLabel labelTargetFloor;
+
     private JLabel labelTime;
+    private int vspace = 10;
+    private int hspace = 5;
 
-    /** konstruktor  drugiego okna */
+
+    /** Konstruktor
+     * @param numberOfFloors
+     * @param numberOfElevators
+     * */
     FormAbout( int numberOfFloors, int numberOfElevators)
     {
         Toolkit kit=Toolkit.getDefaultToolkit();
@@ -25,6 +40,53 @@ class FormAbout extends JFrame{
         labelTime.setBounds(10,10,150,20);
         add(labelTime);
 
+        CreateElevatorFiled(numberOfElevators);
+
         Simulator simulator = new Simulator(numberOfFloors, numberOfElevators);
+    }
+
+    /** Metoda tworząca tabelę textfiledów wind*/
+    private void CreateElevatorFiled(int numberOfElevators){
+        // Szerokość pola tekstowego
+        int width = 120;
+        // Wysokość pola tekstowego
+        int height  = 20;
+
+        labelInformation = new JLabel("Informacje");
+        labelInformation.setBounds(width,30,width,height);
+        add(labelInformation);
+
+        labelPassengers = new JLabel("Liczba pasażerów");
+        labelPassengers.setBounds(width+(width+vspace),30,width,height);
+        add(labelPassengers);
+
+        labelTargetFloor = new JLabel("Docelowe piętro");
+        labelTargetFloor.setBounds(width+(width+vspace)*2,30,width,height);
+        add(labelTargetFloor);
+
+        for(int i = 0; i < numberOfElevators; i++) {
+
+            labelTime = new JLabel("Winda nr " + Integer.toString(i+1) + ":" );
+            labelTime.setBounds(10,60 + i*(hspace+height),80,height);
+            add(labelTime);
+
+            // Pole tekstowe informacji o windzie
+            listFiledsElevator.add(new JTextField());
+            listFiledsElevator.get(i).setBounds(width,60+i*(hspace+height),width,height);
+            listFiledsElevator.get(i).setEditable(false);
+            add(listFiledsElevator.get(i));
+
+            // Pole tekstowe informacji o liczbie pasażerów windy
+            listFiledsNumberOfPassengers.add(new JTextField());
+            listFiledsNumberOfPassengers.get(i).setBounds(width+(width+vspace),60+i*(hspace+height),width,height);
+            listFiledsNumberOfPassengers.get(i).setEditable(false);
+            add(listFiledsNumberOfPassengers.get(i));
+
+            // Pole tekstowe informacji o docelowym piętrze windy
+            listFiledsTargetFloor.add(new JTextField());
+            listFiledsTargetFloor.get(i).setBounds(width+(width+vspace)*2,60+i*(hspace+height),width,height);
+            listFiledsTargetFloor.get(i).setEditable(false);
+            add(listFiledsTargetFloor.get(i));
+        }
     }
 }
