d7f10d143e2db9613dec6766221f8940a858fcac
Stanislaw Franczyk <sfranczyk5@gmail.com>
2019-05-29 20:12:04 +0200

    28.05.

--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -1,13 +1,30 @@
 package simulator;
 
+import javax.swing.Timer;
+import java.awt.event.ActionListener;
+
 /* Klasa symulacji
  * Główna klasa pliku, zawiera funkcją main
   * */
 public class Simulator{
+    private Timer timer;
+    private Building building;
+    private PassengerCreator passengerCreator;
+    private ControlSystem control;
+    private int delay = 1000;
+
+    public int time = 0;
 
     Simulator(int numberOfFloors, int numberOfElevators){
-        Building building = new Building( numberOfFloors, numberOfElevators);
-        ControlSystem control = new ControlSystem();
+        building = new Building( numberOfFloors, numberOfElevators);
+        passengerCreator = new PassengerCreator(numberOfFloors);
+        control = new ControlSystem();
+        timer = new Timer();
+    }
+
+    void StartSimulator(){
+        timer.setDelay(delay);
+        timer.setActionCommand(control.ManageElevators(building));
     }
 
 }
