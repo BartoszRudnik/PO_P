496003bc3f62f76a2c2ee2282bbed0853c1f44c0
michal <michal.grabek2@gmail.com>
2019-06-05 14:54:24 +0200

    dodane komentrarze

--- src/simulator/IElevator.java
+++ src/simulator/IElevator.java
@@ -19,6 +19,8 @@ public interface IElevator {
     int GetCurrentFloor();
 
     int MaxNumberPassangers();
-
+    /**
+     *  @param floor
+     */
     void setCall(int floor);
 }
--- src/simulator/IExit.java
+++ src/simulator/IExit.java
@@ -2,6 +2,9 @@ package simulator;
 
 public interface IExit {
 
+    /**
+     * @param passenger
+     * */
     void LetPassenger(Passanger passenger);
     int GetCurrentFloor();
 }
