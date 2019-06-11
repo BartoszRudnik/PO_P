3feebc810aa79f0f903c78d9cd9e090e47e0725e
Stanislaw Franczyk <sfranczyk5@gmail.com>
2019-06-11 09:32:46 +0200

    11.06.

--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -38,12 +38,12 @@ public class Simulator{
         timer = new Timer(delay, e -> {
             if(!passengerCreator.AddingPassengers( addpassengers, time )){
                 int counter = 0;
-                for( IElevator elevator : elevators){
-                    if(elevator.GetNumberOfFreePlaces() == elevator.MaxNumberPassangers()){
+                for( IFloor floor : floors){
+                    if(floor.GetQueueLength() == 0){
                         counter++;
                     }
                 }
-                if(counter == elevators.size()){
+                if(counter == floors.size()){
                     timer.stop();
                     System.out.println("End");
                 }
